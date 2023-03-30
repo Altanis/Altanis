50a34b507576dfc0502c02cc8bda4bede2a28cf3
CoderSudaWuda <38045884+CoderSudaWuda@users.noreply.github.com>
2023-03-30 10:25:27 -0500

    media queries for homepage

--- src/components/FolderHierarchy.js
+++ src/components/FolderHierarchy.js
@@ -8,7 +8,7 @@ export default function FolderHierarchy({ folders, theme: {theme, hoverBackgroun
                 return (
                     <Fragment key={idx}>
                         <Link to={folder.link}>
-                            <div className={`px-4 flex items-center space-x-2 ${theme} duration-200 hover:text-white ${hoverBackgroundTheme} hover:cursor-pointer rounded-full`}>
+                            <div className={`px-4 flex md:items-center md:space-x-2 ${theme} duration-200 hover:text-white ${hoverBackgroundTheme} hover:cursor-pointer rounded-full`}>
                                 <i className={`px-1 ${folders.length <= 2 ? "text-md sm:text-2xl" : "text-xs sm:text-xl"} ${folder.icon}`}></i>
                                 <div className={`${folders.length <= 2 ? "text-lg sm:text-3xl" : "text-xs sm:text-xl"}`}>{folder.name}</div>
                             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,12 +33,12 @@ export default function IconGroup() {
                         </Link>
                     );
                 })}
-                <div className="flex items-center justify-center h-screen md:flex-col">
-                    <div className="inline-flex text-8xl font-bold">
+                <div className="flex items-center justify-center h-screen flex-col">
+                    <div className="inline-flex text-3xl md:text-8xl font-bold">
                         <span className="mr-3 text-left md:mr-5">I'm</span>
                         <span className="mr-3 text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-green-400 to-blue-600 md:mr-5">Altanis.</span>
                     </div>
-                    <p className="text-lg text-center underline decoration-yellow-500 pb-14 w-[486.42px]">I'm a freshman who enjoys programming; specifically game & full stack development, as well as reverse engineering games.</p>
+                    <p className="text-sm md:text-lg text-center underline decoration-yellow-500 pb-14 w-[486.42px]">I'm a freshman who enjoys programming; specifically game & full stack development, as well as reverse engineering games.</p>
                     <div className="flex flex-col text-xl font-bold">
                         <Link
                             to="/projects"
