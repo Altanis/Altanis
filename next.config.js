ed4e04676f37337b2ba321874679d81bcf3463a0
Altanis <38045884+Altanis@users.noreply.github.com>
2024-06-24 02:17:04 +0000

    remove outdated website code

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:
-    {
-        domains: ["i.scdn.co", "github-readme-stats.vercel.app"]
-    }
-}
-
-module.exports = nextConfig
